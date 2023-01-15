9d862f73a3a38221c03d5c85a933fc0bfc85aefd
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2023-01-14 19:01:35 -0500

    Add files via upload

--- CloverWindows/custom/CloverTask.ps1
+++ CloverWindows/custom/CloverTask.ps1
@@ -2,10 +2,14 @@
 # put a sleep delay so it doesnt interfere with rEFInd if it is present.
 # but ideally the rEFInd scheduled task should be disabled.
 # sleep 60
-$clover = bcdedit.exe /enum firmware | Select-String -pattern cloverx64.efi -Context 2 | findstr identifier ; `
-$clover = $clover -replace 'identifier' ; $clover = $clover -replace ' '
+
+# filter for cloverx64.efi GUID
+$clover = bcdedit.exe /enum firmware | Select-String -pattern cloverx64.efi -Context 2 | findstr "{" ; `
+$clover = $clover -replace '.*\{' -replace '\}.*'
+
+# set Clover GUID to be the next bootsequence
 bcdedit /set "{fwbootmgr}" bootsequence "$clover" /addfirst
-#
+
 # create log file for troubleshooting
 "*** Clover log file for troubleshooting ***" | out-file C:\1Clover-tools\status.txt
 "Provide the contents of this text file when reporting issues." | out-file -append C:\1Clover-tools\status.txt
