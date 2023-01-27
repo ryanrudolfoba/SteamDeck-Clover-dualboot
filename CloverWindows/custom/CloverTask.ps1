e92d08b927c9834d3ac6ad1f38dc01f8aabe33f5
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2023-01-27 09:06:31 -0500

    Add files via upload

--- CloverWindows/custom/CloverTask.ps1
+++ CloverWindows/custom/CloverTask.ps1
@@ -2,17 +2,19 @@
 # put a sleep delay so it doesnt interfere with rEFInd if it is present.
 # but ideally the rEFInd scheduled task should be disabled.
 # sleep 60
-
-# filter for cloverx64.efi GUID
-$clover = bcdedit.exe /enum firmware | Select-String -pattern cloverx64.efi -Context 2 | findstr "{" ; `
-$clover = $clover -replace '.*\{' -replace '\}.*'
-
-# set Clover GUID to be the next bootsequence
-bcdedit /set "{fwbootmgr}" bootsequence "{$clover}" /addfirst
-
+#
+# some crazy string manipulation to filter for the SteamOS EFI entry
+$CloverStatus="C:\1Clover-tools\status.txt"
+$CloverTmp="C:\1Clover-tools\CloverTmp.txt"
+$queryClover = bcdedit.exe /enum firmware | Select-String -pattern cloverx64.efi -Context 2 | out-file $CloverTmp
+$Clover = get-content $CloverTmp | select-string -pattern Volume -context 2 | findstr "{" ; `
+$Clover = $Clover -replace '.*\{' -replace '\}.*'
+rm $CloverTmp
+bcdedit /set "{fwbootmgr}" bootsequence "{$Clover}" /addfirst
+#
 # create log file for troubleshooting
-"*** Clover log file for troubleshooting ***" | out-file C:\1Clover-tools\status.txt
-"Provide the contents of this text file when reporting issues." | out-file -append C:\1Clover-tools\status.txt
-get-date | out-file -append C:\1Clover-tools\status.txt
-"Clover GUID $clover" | out-file -append C:\1Clover-tools\status.txt
-bcdedit /enum firmware | Select-String -pattern bootsequence | out-file -append C:\1Clover-tools\status.txt
+"*** Clover log file for troubleshooting ***" | out-file $CloverStatus
+"Provide the contents of this text file when reporting issues." | out-file -append $CloverStatus
+get-date | out-file -append $CloverStatus
+"Clover GUID $Clover" | out-file -append $CloverStatus
+bcdedit /enum firmware | Select-String -pattern bootsequence | out-file -append $CloverStatus
