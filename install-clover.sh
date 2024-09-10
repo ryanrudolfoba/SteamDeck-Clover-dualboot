ce151318dc11044134042cf7d9e75bd1a9552bbb
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2024-09-10 18:15:41 -0400

    Add files via upload

--- install-clover.sh
+++ install-clover.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
+clear
+
+
+echo Clover Dual Boot Install Script for SteamOS and Bazzite by ryanrudolf
+echo https://github.com/ryanrudolfoba/SteamDeck-Clover-dualboot
+echo YT - 10MinuteSteamDeckGamer \| https://youtube.com/\@10MinuteSteamDeckGamer
+echo Doing preliminary sanity checks ...
+sleep 2
+
+# check if running on Steam Deck OLED or LCD
+if [ "$(cat /sys/class/dmi/id/board_name)" = "Jupiter" ] || [ "$(cat /sys/class/dmi/id/board_name)" = "Galileo" ]
+then
+	echo Script is running on supported model - Steam Deck $(cat /sys/class/dmi/id/board_name).
+else
+	echo Script is not running on a Steam Deck.
+	echo Script is only tested on a Steam Deck running on SteamOS or Bazzite.
+	exit
+fi
+
+# check if Bazzite or SteamOS
+grep -i bazzite /etc/os-release &> /dev/null
+if [ $? -eq 0 ]
+then
+	OS=bazzite
+	EFI_PATH=/boot/efi/EFI
+	BOOTX64=$EFI_PATH/BOOT/BOOTX64.EFI
+	echo Script is running on supported OS - $OS.
+else
+	grep -i SteamOS /etc/os-release &> /dev/null
+	if [ $? -eq 0 ]
+	then
+		OS=SteamOS
+		EFI_PATH=/esp/efi
+		BOOTX64=$EFI_PATH/boot/bootx64.efi
+		echo Script is running on supported OS - $OS.
+	else
+		echo This is neither Bazzite nor SteamOS!
+		echo Exiting immediately!
+		exit
+	fi
+fi
+
 # define variables here
 CLOVER=$(efibootmgr | grep -i Clover | colrm 9 | colrm 1 4)
-STEAMOS=$(efibootmgr | grep -i SteamOS | colrm 9 | colrm 1 4)
 REFIND=$(efibootmgr | grep -i rEFInd | colrm 9 | colrm 1 4)
+ESP=$(df /dev/nvme0n1p1 --output=avail | tail -n1)
 CLOVER_VERSION=5159
 CLOVER_URL=https://github.com/CloverHackyColor/CloverBootloader/releases/download/$CLOVER_VERSION/Clover-$CLOVER_VERSION-X64.iso.7z
 CLOVER_ARCHIVE=$(curl -s -O -L -w "%{filename_effective}" $CLOVER_URL)
 CLOVER_BASE=$(basename -s .7z $CLOVER_ARCHIVE)
-
-clear
-
-echo Clover Dual Boot Install Script by ryanrudolf
-echo https://github.com/ryanrudolfoba/SteamDeck-Clover-dualboot
-echo YT - 10MinuteSteamDeckGamer
-sleep 2
+CLOVER_EFI=\\EFI\\clover\\cloverx64.efi
 
 if [ "$(passwd --status $(whoami) | tr -s " " | cut -d " " -f 2)" == "P" ]
 then
 	read -s -p "Please enter current sudo password: " current_password ; echo
 	echo Checking if the sudo password is correct.
-	echo -e "$current_password\n" | sudo -S -k ls &> /dev/null
+	echo -e "$current_password\n" | sudo -S ls &> /dev/null
 
 	if [ $? -eq 0 ]
 	then
@@ -35,7 +71,7 @@ else
 	exit
 fi
 
-# sanity check - is there enough spoace on esp
+# sanity check - is there enough space on esp
 mkdir ~/temp-ESP
 echo -e "$current_password\n" | sudo -S mount /dev/nvme0n1p1 ~/temp-ESP
 if [ $? -eq 0 ]
@@ -47,7 +83,6 @@ else
 	exit
 fi
 
-ESP=$(df /dev/nvme0n1p1 --output=avail | tail -n1)
 if [ $ESP -ge 15000 ]
 then
 	echo ESP partition has $ESP KB free space.
@@ -57,7 +92,7 @@ then
 else
 	echo ESP partition has $ESP KB free space.
 	echo Not enough space on the ESP partition!
-	echo -e "$current_password\n" | sudo -S du -hd2 /esp
+ 	echo -e "$current_password\n" | sudo -S du -hd2 /esp
 	echo -e "$current_password\n" | sudo -S umount ~/temp-ESP
 	rmdir ~/temp-ESP
 	exit
@@ -75,7 +110,7 @@ else
 		echo -e "$current_password\n" | sudo -S efibootmgr -b $rEFInd_boot -B &> /dev/null
 	done
 	echo -e "$current_password\n" | sudo -S systemctl disable bootnext-refind.service &> /dev/null
-	echo -e "$current_password\n" | sudo -S rm -rf /esp/efi/refind &> /dev/null
+	echo -e "$current_password\n" | sudo -S rm -rf $EFI_PATH/refind &> /dev/null
 	echo -e "$current_password\n" | sudo -S rm /etc/systemd/system/bootnext-refind.service &> /dev/null
 	rm -rf ~/.SteamDeck_rEFInd &> /dev/null
 
@@ -91,7 +126,7 @@ else
 fi
 
 # obtain Clover ISO
-/usr/bin/7z x $CLOVER_ARCHIVE -aoa $CLOVER_BASE &> /dev/null
+7z x $CLOVER_ARCHIVE -aoa $CLOVER_BASE &> /dev/null
 if [ $? -eq 0 ]
 then
 	echo Clover has been downloaded from the github repo!
@@ -114,9 +149,9 @@ else
 fi
 
 # copy Clover files to EFI system partition
-echo -e "$current_password\n" | sudo -S cp -Rf ~/temp-clover/efi/clover /esp/efi/
-echo -e "$current_password\n" | sudo -S cp custom/config.plist /esp/efi/clover/config.plist 
-echo -e "$current_password\n" | sudo -S cp -Rf custom/themes/* /esp/efi/clover/themes
+echo -e "$current_password\n" | sudo -S cp -Rf ~/temp-clover/efi/clover $EFI_PATH
+echo -e "$current_password\n" | sudo -S cp custom/config.plist $EFI_PATH/clover/config.plist
+echo -e "$current_password\n" | sudo -S cp -Rf custom/themes/* $EFI_PATH/clover/themes
 
 # delete temp directories created and delete the Clover ISO
 echo -e "$current_password\n" | sudo -S umount ~/temp-clover
@@ -130,17 +165,43 @@ do
 done
 
 # install Clover to the EFI system partition
-echo -e "$current_password\n" | sudo -S efibootmgr -c -d /dev/nvme0n1 -p 1 -L "Clover - GUI Boot Manager" -l "\EFI\clover\cloverx64.efi" &> /dev/null
-echo -e "$current_password\n" | sudo -S mv /esp/efi/boot/bootx64.efi /esp/efi/boot/bootx64.efi.orig 
-echo -e "$current_password\n" | sudo -S cp /esp/efi/clover/cloverx64.efi /esp/efi/boot/bootx64.efi
+echo -e "$current_password\n" | sudo -S efibootmgr -c -d /dev/nvme0n1 -p 1 -L "Clover - GUI Boot Manager" -l "$CLOVER_EFI" &> /dev/null
 
-# Backup and disable the Windows EFI entry!
-echo -e "$current_password\n" | sudo -S cp /esp/efi/Microsoft/Boot/bootmgfw.efi /esp/efi/Microsoft/Boot/bootmgfw.efi.orig &> /dev/null
-echo -e "$current_password\n" | sudo -S mv /esp/efi/Microsoft/Boot/bootmgfw.efi /esp/efi/Microsoft &> /dev/null
+# check if bootx64.efi.orig already exists
+echo -e "$current_password\n" | sudo -S test -e $BOOTX64.orig
+if [ $? -eq 0 ]
+then
+	echo $BOOTX64.orig found - no action needed.
+else
+	echo $BOOTX64 backup not found.
+	echo -e "$current_password\n" | sudo -S cp $BOOTX64 $BOOTX64.orig
+	echo -e "$current_password\n" | sudo -S cp $EFI_PATH/clover/cloverx64.efi $BOOTX64
+	echo Copy Clover EFI to $BOOTX64 - done.
+fi
+
+# check if Windows EFI needs to be disabled!
+echo -e "$current_password\n" | sudo -S test -e $EFI_PATH/Microsoft/Boot/bootmgfw.efi.orig
+if [ $? -eq 0 ]
+then
+	echo Windows EFI backup exists. Check if Windows EFI needs to be disabled.
+	echo -e "$current_password\n" | sudo -S test -e $EFI_PATH/Microsoft/Boot/bootmgfw.efi
+	if [ $? -eq 0 ]
+	then
+		echo -e "$current_password\n" | sudo -S mv $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/bootmgfw.efi &> /dev/null
+		echo Windows EFI needs to be disabled - done.
+	else
+		echo Windows EFI is already disabled - no action needed.
+	fi
+else
+	echo Windows EFI backup does not exist.
+	echo -e "$current_password\n" | sudo -S cp $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/Boot/bootmgfw.efi.orig &> /dev/null
+	echo -e "$current_password\n" | sudo -S mv $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/bootmgfw.efi &> /dev/null
+	echo Windows EFI needs to be disabled - done.
+fi
 
 # re-arrange the boot order and make Clover the priority!
 echo -e "$current_password\n" | sudo -S efibootmgr -n $CLOVER &> /dev/null
-echo -e "$current_password\n" | sudo -S efibootmgr -o $CLOVER,$STEAMOS &> /dev/null
+echo -e "$current_password\n" | sudo -S efibootmgr -o $CLOVER &> /dev/null
 
 # Final sanity check
 efibootmgr | grep "Clover - GUI" &> /dev/null
@@ -159,7 +220,6 @@ cp custom/Clover-Toolbox.sh ~/1Clover-tools &> /dev/null
 echo -e "$current_password\n" | sudo -S cp custom/clover-bootmanager.service custom/clover-bootmanager.sh /etc/systemd/system
 cp -R custom/logos ~/1Clover-tools &> /dev/null
 cp -R custom/efi ~/1Clover-tools &> /dev/null
-#sudo cp ~/1Clover-tools/logos/SteamDeckLogo.png /esp/efi/steamos/steamos.png &> /dev/null
 
 # make the scripts executable
 chmod +x ~/1Clover-tools/Clover-Toolbox.sh
@@ -170,12 +230,27 @@ echo -e "$current_password\n" | sudo -S systemctl daemon-reload
 echo -e "$current_password\n" | sudo -S systemctl enable --now clover-bootmanager.service
 echo -e "$current_password\n" | sudo -S /etc/systemd/system/clover-bootmanager.sh
 
-# copy dolphin root extension to easily add themes
-mkdir -p ~/.local/share/kservices5/ServiceMenus
-cp custom/open_as_root.desktop ~/.local/share/kservices5/ServiceMenus
+# custom config if using SteamOS or Bazzite
+if [ $OS = SteamOS ]
+then
+	mkdir -p ~/.local/share/kservices5/ServiceMenus
+	cp custom/open_as_root.desktop ~/.local/share/kservices5/ServiceMenus
+else
+	echo -e "$current_password\n" | sudo -S blkid | grep nvme0n1p1 | grep esp &> /dev/null
+	if [ $? -eq 0 ]
+	then
+		echo ESP partition is already labeled - no action needed.
+	else
+		echo -e "$current_password\n" | sudo -S fatlabel /dev/nvme0n1p1 esp &> /dev/null
+		echo ESP partition label has been completed.
+	fi
+
+	# set bazzite as the default boot in Clover config
+	echo -e "$current_password\n" | sudo -S sed -i '/<key>DefaultLoader<\/key>/!b;n;c\\t\t<string>\\EFI\\FEDORA\\SHIMX64\.efi<\/string>' $EFI_PATH/clover/config.plist
+fi
 
 # create desktop icon for Clover Toolbox
 ln -s ~/1Clover-tools/Clover-Toolbox.sh ~/Desktop/Clover-Toolbox &> /dev/null
 echo -e Desktop icon for Clover Toolbox has been created!
 
-echo Clover install completed!
+echo Clover install completed on $OS!
