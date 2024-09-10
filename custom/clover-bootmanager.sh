5754a36f57fdfd3d0731c13467b66ffb88e4c8ea
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2024-09-10 18:16:29 -0400

    support for Bazzite and SteamOS

--- custom/clover-bootmanager.sh
+++ custom/clover-bootmanager.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
 
-CloverStatus=/home/deck/1Clover-tools/status.txt
+# define variables here
+CLOVER=$(efibootmgr | grep -i Clover | colrm 9 | colrm 1 4)
+CLOVER_EFI=\\EFI\\clover\\cloverx64.efi
+ESP_PARTITION=$(df -h | grep nvme0n1p1 | tr -s " " | cut -d " " -f 1)
+ESP_MOUNT_POINT=$(df -h | grep nvme0n1p1 | tr -s " " | cut -d " " -f 6)
+ESP_ALLOCATED_SPACE=$(df -h | grep nvme0n1p1 | tr -s " " | cut -d " " -f 2)
+ESP_USED_SPACE=$(df -h | grep nvme0n1p1 | tr -s " " | cut -d " " -f 3)
+ESP_FREE_SPACE=$(df -h | grep nvme0n1p1 | tr -s " " | cut -d " " -f 4)
 
-echo Clover Boot Manager - $(date) > $CloverStatus
-echo BIOS Version : $(sudo dmidecode -s bios-version) >> $CloverStatus
-cat /etc/os-release | grep 'PRETTY_NAME\|VERSION_ID\|BUILD_ID' >> $CloverStatus
-uname -a >> $CloverStatus
+# check if Bazzite or SteamOS
+grep -i bazzite /etc/os-release &> /dev/null
+if [ $? -eq 0 ]
+then
+	OS=bazzite
+	EFI_PATH=/boot/efi/EFI
+	EFI_NAME=\\EFI\\fedora\\shimx64.efi
+	OWNER=bazzite\:bazzite
+	CloverStatus=/home/bazzite/1Clover-tools/status.txt
+	echo Script is running on supported OS - $OS! > $CloverStatus
+else
+	grep -i SteamOS /etc/os-release &> /dev/null
+	if [ $? -eq 0 ]
+	then
+		OS=SteamOS
+		EFI_PATH=/esp/efi
+		EFI_NAME=\\EFI\\steamos\\steamcl.efi
+		OWNER=deck\:deck
+		CloverStatus=/home/deck/1Clover-tools/status.txt
+		echo Script is running on supported OS - $OS! > $CloverStatus
+	else
+		echo This is neither Bazzite nor SteamOS! > $CloverStatus
+		echo Exiting immediately! >> $CloverStatus
+		exit
+	fi
+fi
+
+echo Clover Boot Manager - $(date) >> $CloverStatus
+echo BIOS Version : $(cat /sys/class/dmi/id/bios_version) >> $CloverStatus
+
+if [ $OS = bazzite ]
+then
+	echo OS Name : $(grep PRETTY_NAME /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+	echo OS Version : $(grep OSTREE_VERSION /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+	echo OS Build : $(grep BUILD_ID /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+else
+	echo OS Name : $(grep PRETTY_NAME /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+	echo OS Version : $(grep VERSION_ID /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+	echo OS Build : $(grep BUILD_ID /etc/os-release | cut -d "=" -f 2) >> $CloverStatus
+fi
+
+echo Kernel Version : $(uname -a) >> $CloverStatus
 
 # check for dump files
 dumpfiles=$(ls -l /sys/firmware/efi/efivars/dump-type* 2> /dev/null | wc -l)
 
 if [ $dumpfiles -gt 0 ]
 then
-	echo dump files exists. performing cleanup. >> $CloverStatus
+	echo EFI dump files exists - cleanup completed. >> $CloverStatus
 	sudo rm -f /sys/firmware/efi/efivars/dump-type*
 else
-	echo no dump files. no action needed. >> $CloverStatus
+	echo EFI dump files does not exist - no action needed. >> $CloverStatus
 fi
 
 # Sanity Check - are the needed EFI entries available?
@@ -25,28 +70,48 @@ then
 	echo Clover EFI entry exists! No need to re-add Clover. >> $CloverStatus
 else
 	echo Clover EFI entry is not found. Need to re-ad Clover. >> $CloverStatus
-	efibootmgr -c -d /dev/nvme0n1 -p 1 -L "Clover - GUI Boot Manager" -l "\EFI\clover\cloverx64.efi" &> /dev/null
+	efibootmgr -c -d /dev/nvme0n1 -p 1 -L "Clover - GUI Boot Manager" -l "$CLOVER_EFI" &> /dev/null
 fi
 
-efibootmgr | grep -i Steam &> /dev/null
+efibootmgr | grep -i $OS &> /dev/null
 if [ $? -eq 0 ]
 then
-	echo SteamOS EFI entry exists! No need to re-add SteamOS. >> $CloverStatus
+	echo $OS EFI entry exists! No need to re-add $OS. >> $CloverStatus
 else
-	echo SteamOS EFI entry is not found. Need to re-add SteamOS. >> $CloverStatus
-	efibootmgr -c -d /dev/nvme0n1 -p 1 -L "SteamOS" -l "\EFI\steamos\steamcl.efi" &> /dev/null
+	echo SteamOS EFI entry is not found. Need to re-add $OS. >> $CloverStatus
+	efibootmgr -c -d /dev/nvme0n1 -p 1 -L "$OS" -l "$EFI_NAME" &> /dev/null
 fi
 
-# disable the Windows EFI entry!
-test -f /esp/efi/Microsoft/Boot/bootmgfw.efi && echo Windows EFI exists! Moving it to /esp/efi/Microsoft >> $CloverStatus && sudo cp /esp/efi/Microsoft/Boot/bootmgfw.efi /esp/efi/Microsoft/Boot/bootmgfw.efi.orig && sudo mv /esp/efi/Microsoft/Boot/bootmgfw.efi /esp/efi/Microsoft &>> $CloverStatus
+# check if Windows EFI needs to be disabled!
+if [ -e $EFI_PATH/Microsoft/Boot/bootmgfw.efi.orig ]
+then
+	echo Windows EFI backup exists. Check if Windows EFI needs to be disabled. >> $CloverStatus
+	if [ -e $EFI_PATH/Microsoft/Boot/bootmgfw.efi ]
+	then
+		mv $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/bootmgfw.efi &> /dev/null
+		echo Windows EFI needs to be disabled - done. >> $CloverStatus
+	else
+		echo Windows EFI is already disabled - no action needed. >> $CloverStatus
+	fi
+else
+	echo Windows EFI backup does not exist. >> $CloverStatus
+	cp $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/Boot/bootmgfw.efi.orig &> /dev/null
+	mv $EFI_PATH/Microsoft/Boot/bootmgfw.efi $EFI_PATH/Microsoft/bootmgfw.efi &> /dev/null
+	echo Windows EFI needs to be disabled - done. >> $CloverStatus
+fi
 
 # re-arrange the boot order and make Clover the priority!
 Clover=$(efibootmgr | grep -i Clover | colrm 9 | colrm 1 4)
-SteamOS=$(efibootmgr | grep -i SteamOS | colrm 9 | colrm 1 4)
-efibootmgr -o $Clover,$SteamOS &> /dev/null
+OtherOS=$(efibootmgr | grep -i $OS | colrm 9 | colrm 1 4)
+efibootmgr -o $Clover,$OtherOS &> /dev/null
 
 echo "*** Current state of EFI entries ****" >> $CloverStatus
 efibootmgr | grep -iv 'Boot2\|PXE' >> $CloverStatus
 echo "*** Current state of EFI partition ****" >> $CloverStatus
-df -h | grep -i 'Filesystem\|esp' >> $CloverStatus
-chown deck:deck $CloverStatus
+echo ESP partition: $ESP_PARTITION >> $CloverStatus
+echo ESP mount point: $ESP_MOUNT_POINT >> $CloverStatus
+echo ESP allocated space: $ESP_ALLOCATED_SPACE >> $CloverStatus
+echo ESP used space: $ESP_USED_SPACE >> $CloverStatus
+echo ESP free space: $ESP_FREE_SPACE >> $CloverStatus
+
+chown $OWNER $CloverStatus
