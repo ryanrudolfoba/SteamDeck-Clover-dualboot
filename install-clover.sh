0107847ae2a79f205c515d14aad926d840f455bc
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2025-07-07 14:10:08 -0400

    support Asus ROG Ally

--- install-clover.sh
+++ install-clover.sh
@@ -29,6 +29,14 @@ then
 	echo Creating config specific for Legion GO
 	sed -i '/<key>Enabled<\/key>/!b;n;c\\t\t\t<true\/>' custom/config.plist
 	sed -i '/<key>ScreenResolution<\/key>/!b;n;c\\t\t<string>2560x1260<\/string>' custom/config.plist
+
+# check if running on Asus ROG Ally
+elif [ "$(cat /sys/class/dmi/id/board_name)" = "RC71L" ]
+then
+	echo Script is running on supported model - Asus ROG Ally $(cat /sys/class/dmi/id/board_name).
+	echo Creating config specific for Asus ROG Ally
+	sed -i '/<key>Enabled<\/key>/!b;n;c\\t\t\t<true\/>' custom/config.plist
+	sed -i '/<key>ScreenResolution<\/key>/!b;n;c\\t\t<string>1920x1080<\/string>' custom/config.plist
 else
 	echo Unsupported device! Exiting immediately.
 	exit
